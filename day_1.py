12b67c88dc2af9ebb8ed94f0eb59743e733c8df3
Yuvan Vishnu Pandi <71508816+yuvanvishnupandi@users.noreply.github.com>
2025-01-01 14:23:41 +0530

    Add files via upload

---
+++ day_1.py
@@ -0,0 +1,9 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+input_int = int(input())
+input_double = float(input())
+input_string = input()
+print(i + input_int)
+print(f"{d + input_double:.1f}")
+print(s + input_string)
