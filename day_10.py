806df8e6c38971989bb6d2a701292e1eef7ed032
Yuvan Vishnu Pandi <71508816+yuvanvishnupandi@users.noreply.github.com>
2025-01-04 10:06:59 +0530

    Update day_10.py

--- day_10.py
+++ day_10.py
@@ -1,6 +1,6 @@
 if __name__ == '__main__':
     n = int(input().strip()) 
     binary_rep = bin(n)[2:] 
-    max_consecutive_ones = max(len(group) for group in binary_rep.split('0'))  
+    max_consecutive_ones = max(len(group) 
+    for group in binary_rep.split('0'))  
     print(max_consecutive_ones) 
-5
